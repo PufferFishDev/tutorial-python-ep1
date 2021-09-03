689a74f2eb4487e3355d2e527a1b41792c2f7158
PufferFishMan <80381307+PufferFishDev@users.noreply.github.com>
2021-09-03 08:48:54 +0200

    Add files via upload

---
+++ tuturial.py
@@ -0,0 +1,25 @@
+
+#How to say something
+# str
+# print("Hello, World!")
+# int
+# print(1)
+# bool
+# print(True)
+
+# this stores the str "Name"
+# name = "Name"
+
+#INPUT
+#input("Whats youre name?: ")
+
+#Says hello to the name 
+
+# name = input("Enter Your Name: ")
+
+# print("Hello, " + name + " Welcome!")
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+print(num1 + num2)
